0380a440dc3c418f9020859470f80e588322ff85
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-10-31 21:40:47 +0530

    TOC

---
+++ TOC/CFG.py
@@ -0,0 +1,26 @@
+import nltk
+from nltk import CFG
+
+grammar=CFG.fromstring("""
+    S -> 'a'S'b' | ''               
+""")
+
+#Display the grammar
+print(grammar)
+
+#Initialize the parser with the grammar
+parser=nltk.ChartParser(grammar)
+
+test_strings=["","ab","aabb","aaabbb"]
+
+for sentence in test_strings:
+    tokens=list(sentence)
+
+    print(f"Parsing {sentence} : ")
+
+    try:
+        for tree in parser.parse(tokens):
+            print(tree)
+            tree.pretty_print()
+    except ValueError:
+        print("NO Valid Parse Found")
